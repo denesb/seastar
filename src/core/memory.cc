a5eb5bf2ce9b3409f948b6bc4ee3288adaf888a5
Botond Dénes <bdenes@scylladb.com>
2020-09-28 16:33:35 +0300

    memory: do_dump_memory_diagnostics(): print to std::ostream&
    Instead of logging each line separately, take an `std::ostream&` and
    write into that. This allows abstracting away the final destination of
    the report as well as collecting all the output into a single
    `std::ostringstream` and logging it as a single message, helping to
    ensure the report is not interwoven with other log lines.
    For now we just to the latter -- collect all the output into a single
    `std::ostringstream` and log it as a single message. To make this a
    little more readable the report is adjusted -- the first log line just
    explains that a detailed report will follow, the report proper starts on
    the next new line. Formatting of the report itself is not changed.

--- src/core/memory.cc
+++ src/core/memory.cc
@@ -198,7 +198,7 @@ class page_list_link {
     uint32_t _prev;
     uint32_t _next;
     friend class page_list;
-    friend void do_dump_memory_diagnostics();
+    friend void do_dump_memory_diagnostics(std::ostream&);
 };
 
 static char* mem_base() {
@@ -287,7 +287,7 @@ public:
         }
         _front = ary[_front].link._next;
     }
-    friend void do_dump_memory_diagnostics();
+    friend void do_dump_memory_diagnostics(std::ostream&);
 };
 
 class small_pool {
@@ -317,7 +317,7 @@ public:
 private:
     void add_more_objects();
     void trim_free_list();
-    friend void do_dump_memory_diagnostics();
+    friend void do_dump_memory_diagnostics(std::ostream&);
 };
 
 // index 0b0001'1100 -> size (1 << 4) + 0b11 << (4 - 2)
@@ -1437,12 +1437,18 @@ void enable_abort_on_allocation_failure() {
     abort_on_allocation_failure.store(true, std::memory_order_seq_cst);
 }
 
-void do_dump_memory_diagnostics() {
+void do_dump_memory_diagnostics(std::ostream& os) {
     auto free_mem = cpu_mem.nr_free_pages * page_size;
     auto total_mem = cpu_mem.nr_pages * page_size;
-    seastar_memory_logger.debug("Used memory: {} Free memory: {} Total memory: {}", total_mem - free_mem, free_mem, total_mem);
-    seastar_memory_logger.debug("Small pools:");
-    seastar_memory_logger.debug("objsz spansz usedobj   memory       wst%");
+    os << "Dumping seastar memory diagnostics\n";
+
+    os << "Used memory: " << total_mem - free_mem
+            << " Free memory: " << free_mem
+            << " Total memory: " << total_mem
+            << "\n";
+
+    os << "Small pools:\n";
+    os << "objsz spansz usedobj   memory       wst%\n";
     for (unsigned i = 0; i < cpu_mem.small_pools.nr_small_pools; i++) {
         auto& sp = cpu_mem.small_pools[i];
         if (sp.object_size() < sizeof(free_object)) {
@@ -1451,10 +1457,14 @@ void do_dump_memory_diagnostics() {
         auto use_count = sp._pages_in_use * page_size / sp.object_size() - sp._free_count;
         auto memory = sp._pages_in_use * page_size;
         auto wasted_percent = memory ? sp._free_count * sp.object_size() * 100.0 / memory : 0;
-        seastar_memory_logger.debug("{} {} {} {} {}", sp.object_size(), sp._span_sizes.preferred * page_size, use_count, memory, wasted_percent);
+        os << sp.object_size() << " "
+                << sp._span_sizes.preferred * page_size << " "
+                << use_count << " "
+                << memory << " "
+                << wasted_percent << "\n";
     }
-    seastar_memory_logger.debug("Page spans:");
-    seastar_memory_logger.debug("index size [B]     free [B]");
+    os << "Page spans:\n";
+    os << "index size [B]     free [B]\n";
     for (unsigned i = 0; i< cpu_mem.nr_span_lists; i++) {
         auto& span_list = cpu_mem.free_spans[i];
         auto front = span_list._front;
@@ -1464,7 +1474,9 @@ void do_dump_memory_diagnostics() {
             total += span.span_size;
             front = span.link._next;
         }
-        seastar_memory_logger.debug("{} {} {}", i, (uint64_t(1) << i) * page_size, total * page_size);
+        os << i << " "
+                << (uint64_t(1) << i) * page_size << " "
+                << total * page_size << "\n";
     }
 }
 
@@ -1475,7 +1487,9 @@ void on_allocation_failure(size_t size) {
                     (seastar_memory_logger.is_enabled(seastar::log_level::debug) && !abort_on_alloc_failure_suppressed))) {
         disable_report_on_alloc_failure_temporarily guard;
         seastar_memory_logger.debug("Failed to allocate {} bytes at {}", size, current_backtrace());
-        do_dump_memory_diagnostics();
+        std::ostringstream os;
+        do_dump_memory_diagnostics(os);
+        seastar_memory_logger.debug(os.str().c_str());
     }
 
     if (!abort_on_alloc_failure_suppressed
