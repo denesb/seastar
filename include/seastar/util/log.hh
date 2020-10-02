b2e8a6abf76b7239054a84073c0644c9e4088ae1
Botond Dénes <bdenes@scylladb.com>
2020-10-02 11:51:15 +0300

    log: introduce log_buf
    A low level component intended to allow efficiently producing the log
    message by allowing the entire message to be formatted into it, without
    any intermediate buffers. It also supports using an external buffer,
    opening the possibility to eliminate allocations for most log messages,
    by using the same static buffer for each log message.
    This patch only introduces log_buf, it will be used in the next patch.

--- include/seastar/util/log.hh
+++ include/seastar/util/log.hh
@@ -65,6 +65,86 @@ namespace seastar {
 class logger;
 class logger_registry;
 
+/// A buffer to format log messages into.
+///
+/// It was designed to allow formatting the entire message into it, without any
+/// intermediary buffers. To minimize the amount of reallocations it supports
+/// using an external buffer. When this is full it moves to using buffers
+/// allocated by itself.
+/// To accommodate the most widely used way of formatting messages -- fmt --,
+/// it provides an output iterator interface for writing into it.
+class log_buf {
+    char* _begin;
+    char* _end;
+    char* _current;
+    bool _own_buf;
+
+private:
+    void free_buffer() noexcept;
+    void realloc_buffer();
+
+public:
+    class inserter_iterator {
+    public:
+        using iterator_category = std::output_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = char;
+        using pointer = char*;
+        using reference = char&;
+
+    private:
+        log_buf* _buf;
+        char* _current;
+
+    public:
+        explicit inserter_iterator(log_buf& buf) noexcept : _buf(&buf), _current(_buf->_current) { }
+        inserter_iterator(const inserter_iterator& o) noexcept : _buf(o._buf), _current(o._current) { }
+
+        reference operator*() {
+            if (__builtin_expect(_current == _buf->_end, false)) {
+                _buf->realloc_buffer();
+                _current = _buf->_current;
+            }
+            return *_current;
+        }
+        inserter_iterator& operator++() noexcept {
+            if (__builtin_expect(_current == _buf->_current, true)) {
+                ++_buf->_current;
+            }
+            ++_current;
+            return *this;
+        }
+        inserter_iterator operator++(int) noexcept {
+            inserter_iterator o(*this);
+            ++(*this);
+            return o;
+        }
+    };
+
+    /// Default ctor.
+    ///
+    /// Allocates an internal buffer of 512 bytes.
+    log_buf();
+    /// External buffer ctor.
+    ///
+    /// Use the external buffer until its full, then switch to internally
+    /// allocated buffer. log_buf doesn't take ownership of the buffer.
+    log_buf(char* external_buf, size_t size) noexcept;
+    ~log_buf();
+    /// Create an output iterator which allows writing into the buffer.
+    inserter_iterator back_insert_begin() noexcept { return inserter_iterator(*this); }
+    /// The amount of data written so far.
+    const size_t size() const noexcept { return _current - _begin; }
+    /// The size of the buffer.
+    const size_t capacity() const noexcept { return _end - _begin; }
+    /// Read only pointer to the buffer.
+    /// Note that the buffer is not guaranteed to be null terminated. The writer
+    /// has to ensure that, should it wish to.
+    const char* data() const noexcept { return _begin; }
+    /// A view of the buffer content.
+    std::string_view view() const noexcept { return std::string_view(_begin, size()); }
+};
+
 /// \brief Logger class for ostream or syslog.
 ///
 /// Java style api for logging.
