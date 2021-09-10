c1e500b4350f2a19d76c31ef48076dcb7f7fd5b6
Botond Dénes <bdenes@scylladb.com>
2021-09-10 14:51:23 +0300

    scollectd: migrate to options api
    Use statically declared structs for configuration instead of
    boost::program_options.

--- src/core/app-template.cc
+++ src/core/app-template.cc
@@ -55,6 +55,7 @@ app_template::seastar_options::seastar_options()
     , reactor_opts(this)
     , metrics_opts(this)
     , smp_opts(this)
+    , scollectd_opts(this)
 {
 }
 
@@ -79,7 +80,6 @@ app_template::app_template(app_template::config cfg)
         _opts.reactor_opts.max_networking_io_control_blocks.set_default_value(cfg.max_networking_aio_io_control_blocks);
         _opts.add_to(_opts_conf_file);
 
-        _opts_conf_file.add(scollectd::get_options_description());
         _opts_conf_file.add(log_cli::get_options_description());
 
         _seastar_opts.add(_opts_conf_file);
@@ -228,7 +228,7 @@ app_template::run_deprecated(int ac, char ** av, std::function<void ()>&& func) 
         return seastar::metrics::configure(_opts.metrics_opts).then([this] {
             // set scollectd use the metrics configuration, so the later
             // need to be set first
-            scollectd::configure( this->configuration());
+            scollectd::configure( _opts.scollectd_opts);
         });
     }).then(
         std::move(func)
