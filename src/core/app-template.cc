50c8c2531c74343940c313723de8536adb70ce91
Botond Dénes <bdenes@scylladb.com>
2021-09-10 14:51:09 +0300

    metrics: migrate to options api
    Use statically declared structs for configuration instead of
    boost::program_options.

--- src/core/app-template.cc
+++ src/core/app-template.cc
@@ -53,6 +53,7 @@ reactor_config_from_app_config(app_template::config cfg) {
 app_template::seastar_options::seastar_options()
     : program_options::option_group(nullptr, "seastar")
     , reactor_opts(this)
+    , metrics_opts(this)
 {
 }
 
@@ -77,7 +78,6 @@ app_template::app_template(app_template::config cfg)
         _opts.reactor_opts.max_networking_io_control_blocks.set_default_value(cfg.max_networking_aio_io_control_blocks);
         _opts.add_to(_opts_conf_file);
 
-        _opts_conf_file.add(seastar::metrics::get_options_description());
         _opts_conf_file.add(smp::get_options_description());
         _opts_conf_file.add(scollectd::get_options_description());
         _opts_conf_file.add(log_cli::get_options_description());
@@ -225,7 +225,7 @@ app_template::run_deprecated(int ac, char ** av, std::function<void ()>&& func) 
     // No need to wait for this future.
     // func is waited on via engine().run()
     (void)engine().when_started().then([this] {
-        return seastar::metrics::configure(this->configuration()).then([this] {
+        return seastar::metrics::configure(_opts.metrics_opts).then([this] {
             // set scollectd use the metrics configuration, so the later
             // need to be set first
             scollectd::configure( this->configuration());
